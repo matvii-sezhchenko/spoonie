499d3ff6d73ffc0992f4c99904a3960d2567c35d
Mattias <matwey.sezchenko@gmail.com>
2026-01-25 00:01:57 +0200

    Add to two function, insert feeding and sleep

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import logging
+from datetime import datetime
 
 def init_db():
 	conn = sqlite3.connect('baby_tracker.db')
@@ -28,3 +29,31 @@ def init_db():
 	conn.commit()
 	conn.close()
 	logging.info("База даних успішно ініціалізована")
+	
+
+def add_feeding(user_name, volume):
+	conn = sqlite3.connect('baby_tracker.db')
+	cursor = conn.cursor()
+
+	now = datetime.now().strftime("%d.%m %H:%M")
+
+	cursor.execute(
+		'INSERT INTO feedings (user_name, volume_ml, timestamp) VALUES (?,?,?)', (user_name, volume, now)
+	)
+
+	conn.commit()
+	conn.close()
+
+
+def add_sleep(user_name, action_type):
+	conn = sqlite3.connect('baby_tracker.db')
+	cursor = conn.cursor()
+
+	now = datetime.now().strftime("%d.%m %H:%M")
+
+	cursor.execute(
+		'INSERT INTO sleep (user_name, start_time) VALUES (?,?)', (user_name, f"{action_type}: {now}")
+	)
+
+	conn.commit()
+	conn.close()
