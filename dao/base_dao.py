fb96c1bd88594d3c73f9ebbae3708b8ee3d47b87
Mattias <matwey.sezchenko@gmail.com>
2026-03-15 18:56:03 +0200

    codereview:  remove a database and migrate from DAO.

--- dao/base_dao.py
+++ dao/base_dao.py
@@ -1,5 +1,8 @@
+import logging
 import sqlite3
+
 from config import DB_NAME
+from schema import TABLES
 
 class BaseDAO:
 	def __init__(self):
@@ -7,3 +10,15 @@ class BaseDAO:
 
 	def get_connection(self):
 		return sqlite3.connect(self._db_name)
+	
+	def initDB (self):
+		try:
+			with self.get_connection() as conn:
+				cursor = conn.cursor()
+				for table_name, sql_query in TABLES.items():
+					cursor.execute(sql_query)
+					logging.info(f'Таблиця {table_name} перевірена/створена.')
+				conn.commit()
+				logging.info("База даних успішно ініціалізована.")
+		except Exception as e:
+			logging.error(f"Помилка ініціалізації БД: {e}")
