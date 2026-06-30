2b975c4f11a70196157053d7914dd370e73ab79f
Mattias <matwey.sezchenko@gmail.com>
2026-06-30 11:59:15 +0300

    feat(repository): implement feeding repository
    Introduces the FeedingRepository class to manage database persistence
    and retrieval of feeding records.
    
    - Implements save method to insert new feeding records
    - Implements get_all method to fetch and map all feeding records
    - Implements delete_by_id method to remove records by ID

---
+++ app/repository/feeding_repository.py
@@ -0,0 +1,41 @@
+import sqlite3
+from typing import List
+from app.repository.database_manager import DatabaseManager
+from app.models.feeding import Feeding
+from app.repository.feeding_queries import INSERT_FEEDING, GET_ALL_FEEDINGS, DELETE_FEEDING
+
+class FeedingRepository:
+    def __init__(self, db_manager: DatabaseManager):
+        self.db_manager = db_manager
+
+    def save (self, feeding: Feeding) -> int:
+        with self.db_manager.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(INSERT_FEEDING, (feeding.user_name, feeding.volume_ml, feeding.timestamp))
+            return cursor.lastrowid
+
+    def get_all(self) -> List[Feeding]:
+        feedings = []
+        with self.db_manager.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(GET_ALL_FEEDINGS)
+            rows = cursor.fetchall()
+
+            for row in rows:
+                feedings_obj = Feeding(
+                    id=row[0],
+                    user_name=row[1],
+                    volume_ml=row[2],
+                    timestamp=row[3]
+                )
+                feedings.append(feedings_obj)
+        
+        return feedings
+
+    def delete_by_id(self, feeding_id: int) -> bool:
+        with self.db_manager.get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(DELETE_FEEDING, (feeding_id))
+            conn.commit()
+
+            return cursor.rowcount > 0
